0112ddc5f297800fd1df54d8405987c411b781c0
Laura Wrubel <lwrubel@gwu.edu>
2016-11-30 10:25:06 -0500

    bumpversion to 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmweiboharvester',
-    version='1.3.0',
+    version='1.3.1',
     url='https://github.com/gwu-libraries/sfm-weibo-harvester',
     author='Vict Tan',
     author_email='ychtan@email.gwu.edu',
@@ -14,7 +14,7 @@ setup(
              'weibo_warc_iter.py'],
     py_modules=['weibo_harvester','weiboarc','weibo_warc_iter'],
     install_requires=['sfmutils'],
-    tests_require=['mock>=1.3.0'],
+    tests_require=['mock>=1.3.1'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
