49b85654d8d83e4e0b6796f34e26858316d5cbce
Justin Littman <justinlittman@gwu.edu>
2017-02-06 09:49:08 -0500

    Bumped version to 1.5.0 and set warcprox to 0.3.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmweiboharvester',
-    version='1.4.3',
+    version='1.5.0',
     url='https://github.com/gwu-libraries/sfm-weibo-harvester',
     author='Vict Tan',
     author_email='ychtan@email.gwu.edu',
