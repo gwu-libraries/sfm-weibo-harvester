416f88817f096b033635abf0665f5f882d702a9b
Justin Littman <justinlittman@gwu.edu>
2017-08-28 10:10:28 -0400

    Bumped version to 1.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmweiboharvester',
-    version='1.9.0',
+    version='1.10.0',
     url='https://github.com/gwu-libraries/sfm-weibo-harvester',
     author='Vict Tan',
     author_email='ychtan@email.gwu.edu',
