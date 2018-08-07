3ce08e6bc25034304acfcd95297f2831b5a17502
Justin Littman <justinlittman@gwu.edu>
2018-08-07 13:51:16 -0400

    Bumped version to 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmweiboharvester',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/gwu-libraries/sfm-weibo-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
