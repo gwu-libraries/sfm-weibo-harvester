c15f766b42808090490ecd84389a223795fa9e74
Dan Kerchner <kerchner@gwu.edu>
2019-08-04 15:27:24 -0400

    Bump version to 2.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmweiboharvester',
-    version='2.1.0',
+    version='2.2.0',
     url='https://github.com/gwu-libraries/sfm-weibo-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
